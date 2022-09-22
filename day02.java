d20aaf28a8c083bc3823f537142ca2cfe490e403
爱你呀，琪佳 <93711629+hbl0123@users.noreply.github.com>
2022-09-22 10:29:22 +0800

    Add files via upload

---
+++ day02.java
@@ -0,0 +1,45 @@
+/*public class day02 {
+    public static void main(String[] args) {
+        double bankBalance = 500.00;
+        //小明存入250元
+        bankBalance = bankBalance + 250;
+        //小明给女朋友买好吃de ,支出100y￥
+        bankBalance = bankBalance - 100;
+        System.out.println(bankBalance);
+    }
+}
+*/
+public class day02{
+    public static void main(String[] args) {
+     //整数
+        int a = 1;
+        int b = -7;
+        byte g = 13;
+        short h = 128;
+        long j = 1111;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(j);
+     //小数
+        float c = 1.00f;
+        float d = 7.00f;
+        double e = 30.00;
+        double f = 70.00;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+     //char
+        String nihao = "ainijiajia";
+     //布尔值
+        System.out.println(true);
+        System.out.println(false);
+
+    //null
+
+    }
+
+
+}
