c3f6b8d82d0e003bea949c8954c90da92bebc57e
爱你呀，琪佳 <93711629+hbl0123@users.noreply.github.com>
2022-09-26 17:30:02 +0800

    Add files via upload

---
+++ test11.java
@@ -0,0 +1,13 @@
+package com.homeWork.test;
+/*
+在实际开发中，需要重复执行的代码，会选择循环实现。
+比如：如左图所示。玩游戏的时候，如果网不好那么会经常断线重连。
+那么断线重连这个逻辑就需要重复执行
+假设现在公司要求，断线重连的业务逻辑最多只写5次。
+请用代码实现。
+ */
+public class test11 {
+    public static void main(String[] args) {
+
+    }
+}
