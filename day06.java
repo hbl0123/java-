10834d2b162e98e40edf24987e0973f58694f9de
my wife,qijia <93711629+hbl0123@users.noreply.github.com>
2022-10-15 10:30:26 +0800

    Add files via upload

--- day06.java
+++ day06.java
@@ -1,4 +1,24 @@
 package com.homeWork.arithmeticoperator;
+//什么是方法         程序中最小的执行单元
+//       优点：提到代码的复用性  提到代码的可维护性
 
+
+//方法的定义格式：
+//          最简单的方法定义和调用                         带参数的犯法定义和调用
+//         格式： public static void 方法名(){            格式：
+//                     方法体 （打包起来的代码）；}
+//         调用： 方法（）                                 调用：
 public class day06 {
+    public static void main(String[] args) {
+        PlayGame();
+        PlayGame();
+    }
+    public static void PlayGame(){
+        System.out.println("选人物");
+        System.out.println("准备开局");
+        System.out.println("对线");
+        System.out.println("崩盘");
+        System.out.println("骂队友");
+        System.out.println("送人头");
+    }
 }
---
+++ test25.java
@@ -0,0 +1,26 @@
+package com.homeWork.test;
+//展示女朋友的信息
+//需求： 定义一个方法，在方法内部打印女朋友的所有信息
+/*public class test25 {
+    public static void main(String[] args) {
+        Girlfriend();
+    }
+    public static void Girlfriend(){
+        System.out.println("jaijia");
+    }
+}*/
+//需求： 定义一个方法，在方法内部定义两个变量
+//      求出他们的和并进行打印
+public class test25 {
+    public static void main(String[] args) {
+        number1();
+    }
+    public static void number1(){
+        int number01 = 10;
+        int number02 = 20;
+
+        int result = number01 + number02;
+        System.out.println(result);
+        }
+
+}
