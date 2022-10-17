777edbc5e858e1905e6efd7bd3d674efe1ebe2fb
my wife,qijia <93711629+hbl0123@users.noreply.github.com>
2022-10-17 16:06:32 +0800

    Add files via upload

--- day06.java
+++ day06.java
@@ -5,10 +5,23 @@ package com.homeWork.arithmeticoperator;
 
 //方法的定义格式：
 //          最简单的方法定义和调用                         带参数的犯法定义和调用
-//         格式： public static void 方法名(){            格式：
+//         格式： public static void 方法名(){            格式：public static void 方法名（参数1，参数2，参数3.。。）{...}
 //                     方法体 （打包起来的代码）；}
-//         调用： 方法（）                                 调用：
-public class day06 {
+//         调用： 方法（）                             调用：方法名（参数，参数2，参数3.。。）
+
+//带返回值的方法定义和调用：
+//public class 返回类型 方法名（参数）{
+//          方法体；
+//          return 返回值；
+//}
+//  直接调用： 方法名（实参）
+//  赋值调用： 整数类型 变量名 = 方法名（实参）；
+//  输出调用： System.out.println(方法名（实参）)；
+
+//形参：全称形式参数，是指方法 定义 中的参数
+//实参：全称实际参数，是指方法 调用 中的参数
+
+/*public class day06 {
     public static void main(String[] args) {
         PlayGame();
         PlayGame();
@@ -22,3 +35,14 @@ public class day06 {
         System.out.println("送人头");
     }
 }
+public class day06 {
+    public static void main(String[] args) {
+        number(10,30);
+    }
+
+    public static void number(int number1,int number2) {
+        int result = number1 + number2;
+        System.out.println(result);
+    }
+}
+ */
--- test25.java
+++ test25.java
@@ -8,7 +8,7 @@ package com.homeWork.test;
     public static void Girlfriend(){
         System.out.println("jaijia");
     }
-}*/
+}
 //需求： 定义一个方法，在方法内部定义两个变量
 //      求出他们的和并进行打印
 public class test25 {
@@ -22,5 +22,64 @@ public class test25 {
         int result = number01 + number02;
         System.out.println(result);
         }
+}
 
+//需求： 定义一个方法，求长方形的周长，将结果在方法中进行打印
+
+public class test25 {
+    public static void main(String[] args) {
+        perimeter(12,34);
+    }
+
+    public static void perimeter(double len,double width){
+            double result = (len + width) * 2;
+            System.out.println(result);
+    }
+}
+//需求： 定义一个方法，求○的面积，将结果在方法中进行打印
+public class test25 {
+    public static void main(String[] args) {
+        area(3.4);
+    }
+
+    public static void area(double radius) {
+        double result = 3.14 * radius * radius;
+        System.out.println(result);
+    }
+}
+
+//需求：
+
+public class test25 {
+    public static void main(String[] args) {
+        double sum1 = sum(321432,43242,434343);
+        double sum2 = sum(32,43,434);
+    }
+    public static double sum(double num1,double num2,double num3){
+        double result = num1 + num2 + num3;
+        return  result;
+    }
+}*/
+
+//定义方法，比较两个长方形的面积
+public class test25 {
+    public static void main(String[] args) {
+        double area1 = area(12,3);
+        double area2 = area(34,4);
+
+        System.out.println(area2);
+        System.out.println(area1);
+        System.out.println(max(area1,area2));
+    }
+    public static double area(double a,double b){
+        double result = a * b;
+        return result;
+    }
+    public static double max(double a, double b){
+        if(a > b){
+            return a;
+        }else{
+            return b;
+        }
+    }
 }
